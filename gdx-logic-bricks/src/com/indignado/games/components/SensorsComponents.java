8c14ef284b2fb6bf276f2fccaca5cbe5213917fe
Rubentxu <rubentxu74@gmail.com>
2014-10-17 10:24:59 +0200

    eliminado data

---
+++ gdx-logic-bricks/src/com/indignado/games/components/SensorsComponents.java
@@ -0,0 +1,14 @@
+package com.indignado.games.components;
+
+import com.badlogic.gdx.utils.Array;
+import com.indignado.games.bricks.sensors.Sensor;
+
+/**
+ * Created on 17/10/14.
+ *
+ * @author Rubentxu
+ */
+public class SensorsComponents {
+    public Array<Sensor> sensors;
+
+}
